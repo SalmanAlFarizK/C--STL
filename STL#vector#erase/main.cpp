87c48f6a6943192a9e918f5806ef0a964a5376fb
SalmanAlFarizK <alfarizksalman@gmail.com>
2024-06-02 17:31:26 +0530

    added one more data into vector for STL#vector#erase folder

--- STL#vector#erase/main.cpp
+++ STL#vector#erase/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    vector<int>v={1,2,3,4,5,6,7};
+    vector<int>v={1,2,3,4,5,6,7,8};
     cout<<"vector array before erasing any element "<<endl;
     for(auto i: v)
     {
